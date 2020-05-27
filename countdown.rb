3b2f17fc60c804fb37e8cbc615921ae4acc9e0fc
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-05-27 18:07:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,4 @@
-def countdown(10)
+def countdown(number=10)
   while number > 0
   puts "#{number} SECOND(S)!"
   number -= 1
@@ -6,7 +6,7 @@ def countdown(10)
 puts "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(10)
+def countdown_with_sleep(number=10)
   while number > 0
   puts "#{number} SECOND(S)!"
   number -= 1
